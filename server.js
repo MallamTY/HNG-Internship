2a3fc1a9336fbe0a0e1631f344e50e61fc3f60a5
MallamTY <sosanyatemitayonurudeen@gmail.com>
2022-10-28 12:13:47 +0100

    Added CORS

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const route = require('./route/route')
 const helmet = require('helmet');
 const compression = require('compression');
+const cors = require('cors');
 const router = express.Router();
 const app = express();
 const {PORT} = require('./configVariable');
 
+
 app.use(helmet());
 app.use(compression());
+app.use(cors());
 app.use('/', route);
 app.use('*', (req, res, next) => {
     res.status(404).send(`Invalid URL supplied !!!!!!!!!!!!!!!`)
